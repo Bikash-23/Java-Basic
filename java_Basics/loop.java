769e7db6bedb472fb2cd61a12fd59a0139e98a3e
Bikash Pari <paribikash23@gmail.com>
2025-08-27 17:54:11 +0000

    loop(for)

--- java_Basics/loop.java
+++ java_Basics/loop.java
@@ -6,8 +6,11 @@ public class loop {
         System.out.println("Enter Value: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i =0;i<n;i++){
-            System.err.println("*\t");
+        for(int i =0;i<=n;i++){
+            for(int j = 0;j<i;j++){
+            System.err.print("* ");
+            }
+            System.out.println();
         }
         sc.close();
     }
